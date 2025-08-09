ff8873587c25e76d50b14b1bb95b77230c154f37
Stanislav Isupov <stanislavisupov@MacBook-Air-Stanislav.local>
2025-08-09 12:07:31 +0300

    init

--- app/main.py
+++ app/main.py
@@ -5,8 +5,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return 'Hello, World 1!'
 
-
+def create_app():
+    return
 if __name__ == '__main__':
     app.run(debug=True)
