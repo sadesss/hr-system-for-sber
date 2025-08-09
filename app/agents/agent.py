ad6427818ac99d90777ab8c1b57de0cd5d78d484
Stanislav Isupov <stanislavisupov@MacBook-Air-Stanislav.local>
2025-08-09 16:15:28 +0300

    reg

--- app/agents/agent.py
+++ app/agents/agent.py
@@ -3,29 +3,30 @@ from langgraph.checkpoint.memory import MemorySaver
 from langchain_gigachat.chat_models import GigaChat
 
 
-text = "https://ngw.devices.sberbank.ru:9443/api/v2/oauth"
+def get_reqest(query, promt):
+    base_url = "https://ngw.devices.sberbank.ru:9443/api/v2/oauth"
 
 
-llm_model = GigaChat(
-    base_url="https://ngw.devices.sberbank.ru:9443/api/v2",
-    access_token="18880c81-bbb1-449e-a277-40edecf6ea38",
-    model='GigaChat-2-Max',
-    verbose=False,
-    temperature=0.1
-)
+    llm_model = GigaChat(
+        base_url=base_url,
+        access_token="18880c81-bbb1-449e-a277-40edecf6ea38",
+        model='GigaChat-2-Max',
+        verbose=False,
+        temperature=0.1
+    )
 
-system_prompt = "Ты банковский помошник"
-agent = create_react_agent(
-    model=llm_model,
-    tools=[],
+    system_prompt = "Ты банковский помошник"
+    agent = create_react_agent(
+        model=llm_model,
+        tools=[],
 
-    prompt=system_prompt,  # Подключаем системный контекст
-    checkpointer=MemorySaver()  # Добавляем объект из библиотеки LangGraph для сохранения памяти агента
-)
+        prompt=system_prompt,  # Подключаем системный контекст
+        checkpointer=MemorySaver()  # Добавляем объект из библиотеки LangGraph для сохранения памяти агента
+    )
 
-query = 'Какова основная цель документа? Опишите ключевые выводы.'
-config = {"configurable": {"thread_id": "main"}}
+    config = {"configurable": {"thread_id": "main"}}
 
-response = agent.invoke({"messages": [("user", query.encode('utf-8', errors='replace').decode('utf-8'))]},
-                        config=config)
-print("🤖 :", response["messages"][-1].content)
+    response = agent.invoke(
+        {"messages": [("user", query.encode('utf-8', errors='replace').decode('utf-8'))]},
+                            config=config)
+    return response["messages"][-1].content
